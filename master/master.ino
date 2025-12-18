872c21499252cda9bce90f55bcaad751755faa90
maxrangner <max@rangner.com>
2025-12-18 23:43:10 +0100

    Finish prototype.
    Goal complete: Send data from master to slave.

--- master/master.ino
+++ master/master.ino
@@ -1,11 +1,20 @@
 #include "WiFi.h"
 #include <esp_now.h>
 #include <esp_wifi.h>
+#include <GawiButtons.h>
 #include "../protocol/protocol.h"
 #include "../src/utils.h"
 
+constexpr int buttonPin = 9;
+
+void onPacketSend(const esp_now_send_info_t *tx_info, esp_now_send_status_t status);
+void updatePacket();
 uint8_t slaveMac[6] = {0x64, 0xe8, 0x33, 0xad, 0x23, 0x64};
-MorseProtocol::Packet myData;
+MorseProtocol::Packet outgoingPacket;
+ButtonManager buttonManager;
+Button* button;
+
+uint8_t payload = 0;
 
 void addPeer(uint8_t* mac) {
   esp_now_peer_info_t peerInfo = {};
@@ -36,10 +45,37 @@ void setup() {
       delay(2000);
     }
   } else Serial.println("ESP-NOW initialized!");
+  esp_now_register_send_cb(onPacketSend);
   addPeer(slaveMac);
+  button = buttonManager.addButton(buttonPin, true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  buttonManager.updateAll();
+  if (button->pressed()) {
+    updatePacket();
+    esp_err_t result = esp_now_send(slaveMac, (uint8_t *) &outgoingPacket, sizeof(MorseProtocol::Packet));
+    if (result == ESP_OK) {
+      Serial.println("Packet sent!");
+    } else Serial.println("Error sending packet.");
+  }
+}
 
+void onPacketSend(const esp_now_send_info_t *tx_info, esp_now_send_status_t status) {
+  if (status == ESP_OK) {
+    Serial.println("Data sent: ");
+    Serial.print("Protocol Version: "); Serial.println(outgoingPacket.protocolVersion);
+    Serial.print("Device ID: "); Serial.println(outgoingPacket.deviceId);
+    Serial.print("Packet Type: "); Serial.println(outgoingPacket.packetType);
+    Serial.print("Payload: "); Serial.println(outgoingPacket.payload);
+  } else {
+    Serial.println("Packet not delivered.");
+  }
+}
+
+void updatePacket() {
+  outgoingPacket.protocolVersion = 1;
+  outgoingPacket.deviceId = 1;
+  outgoingPacket.packetType = MorseProtocol::PacketType::STATUS;
+  outgoingPacket.payload = payload++;
 }
--- slave/slave.ino
+++ slave/slave.ino
@@ -1,18 +1,13 @@
 #include "WiFi.h"
 #include <esp_now.h>
 #include <esp_wifi.h>
+#include "../protocol/protocol.h"
+#include "../src/utils.h"
 
-void getMac() {
-  // Used to configure peer on master
-  uint8_t baseMac[6];
-  esp_err_t returnVal = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
-  if (returnVal == ESP_OK) {
-    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
-    Serial.println("");
-  } else {
-    Serial.println("Failed to get MAC.");
-  }
-}
+constexpr int ledPin = 8;
+
+void onPacketReceive(const esp_now_recv_info *info, const uint8_t *incomingPacket, int len);
+MorseProtocol::Packet receivePacket;
 
 void setup() {
   Serial.begin(115200);
@@ -26,10 +21,36 @@ void setup() {
       delay(2000);
     }
   }
-  getMac();
+  esp_now_register_recv_cb(onPacketReceive);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  delay(100);
+  digitalWrite(ledPin, LOW);
+  delay(100);
+  digitalWrite(ledPin, HIGH);
+  delay(100);
+  digitalWrite(ledPin, LOW);
+  delay(100);
+  digitalWrite(ledPin, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void onPacketReceive(const esp_now_recv_info *info, const uint8_t *incomingPacket, int len) {
+  if (len == sizeof(MorseProtocol::Packet)) {
+    memcpy(&receivePacket, incomingPacket, sizeof(MorseProtocol::Packet));
+    Serial.print("Data received: "); Serial.println(len);
+    Serial.print("Protocol Version: "); Serial.println(receivePacket.protocolVersion);
+    Serial.print("Device ID: "); Serial.println(receivePacket.deviceId);
+    Serial.print("Packet Type: "); Serial.println(receivePacket.packetType);
+    Serial.print("Payload: "); Serial.println(receivePacket.payload);
+    if (receivePacket.payload % 2 == 0) {
+      digitalWrite(ledPin, HIGH);
+    } else digitalWrite(ledPin, LOW);
+  } else {
+    Serial.println("Error with packet.");
+  }
 }
